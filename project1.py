4e3042ee8165d829850c30f9a8c04a7c62f27d00
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-12 11:29:11 +0000

    add player roll, and keep/print score

--- project1.py
+++ project1.py
@@ -7,5 +7,35 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+while True:
+    players = input("Enter the number of players (2-4:): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("Invalid, try again")
+
+max_score = 50
+player_scores = [0 for _ in range(len(players))]
+
+while(player_scores) < max_score:
+
+    for player_idx in range(players):
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)?" )
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 0:
+                print("You rolled a 1! Turn done!")
+            else:
+                print("You rolled a: ", value)
+
+        player_scores[player_idx] += current_score
+        print("Your score is: ", current_score)
