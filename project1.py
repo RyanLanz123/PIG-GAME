8584e985ac4480c8700ca30d505b286d673c5ff4
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-13 13:51:44 +0000

    add full functionality for players to roll and keep trck of scores, announce winner

--- project1.py
+++ project1.py
@@ -19,11 +19,13 @@ while True:
         print("Invalid, try again")
 
 max_score = 50
-player_scores = [0 for _ in range(len(players))]
 
-while(player_scores) < max_score:
+player_scores = [0 for _ in range(players)]
 
+while max(player_scores) < max_score:
     for player_idx in range(players):
+        print("\nPlayer", player_idx + 1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
         current_score = 0
 
         while True:
@@ -32,10 +34,19 @@ while(player_scores) < max_score:
                 break
 
             value = roll()
-            if value == 0:
+            if value == 1:
                 print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
             else:
+                current_score += value
                 print("You rolled a: ", value)
 
+            print("Your score is:", current_score)
+
         player_scores[player_idx] += current_score
-        print("Your score is: ", current_score)
+        print("Your total score is: ", player_scores[player_idx])
+
+max_score= max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player", winning_idx + 1, "is the winner with a score of: ", max_score)
